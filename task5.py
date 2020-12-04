340d8d19dc490fad24bf7c88f0ae0ab19d4e4f77
Andrey Vasichenko <ad.vasilchenko@yandex.ru>
2020-12-04 15:28:04 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,20 +1,17 @@
-from  urllib import request
+from urllib import request
 import json
 import pytest
 from unittest.mock import patch
 
-API_URL = 'http://worldclockapi.com/api/json/utc/now'
+API_URL = "http://worldclockapi.com/api/json/utc/now"
 
-YMD_SEP = '-'
+YMD_SEP = "-"
 YMD_SEP_INDEX = 4
 YMD_YEAR_SLICE = slice(None, YMD_SEP_INDEX)
 
-DMY_SEP = '.'
+DMY_SEP = "."
 DMY_SEP_INDEX = 5
 DMY_YEAR_SLICE = slice(DMY_SEP_INDEX + 1, DMY_SEP_INDEX + 5)
-
-
-
 
 
 def what_is_year_now() -> int:
@@ -28,27 +25,29 @@ def what_is_year_now() -> int:
     with request.urlopen(API_URL) as resp:
         resp_json = json.load(resp)
 
-    datetime_str = resp_json['currentDateTime']
+    datetime_str = resp_json["currentDateTime"]
     if datetime_str[YMD_SEP_INDEX] == YMD_SEP:
         year_str = datetime_str[YMD_YEAR_SLICE]
     elif datetime_str[DMY_SEP_INDEX] == DMY_SEP:
         year_str = datetime_str[DMY_YEAR_SLICE]
     else:
-        raise ValueError('Invalid format')
+        raise ValueError("Invalid format")
 
     return int(year_str)
 
+
 def test_year_ymd():
-	date_mock = '2019-03-01'
-	exp_year = 2019
+    date_mock = "2019-03-01"
+    exp_year = 2019
 
-	with patch.object('request.urlopen', return_value=date_mock):
-		year = request.urlopen('http://worldclockapi.com/api/json/utc/now')
-		#year = what_is_year_now()
+    with patch.object("request.urlopen", return_value=date_mock):
+        year = request.urlopen("http://worldclockapi.com/api/json/utc/now")
+        # year = what_is_year_now()
 
-	assert year == exp_year
+    assert year == exp_year
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     year = what_is_year_now()
     exp_year = 2019
     print(year)
